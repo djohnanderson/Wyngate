724f775b0b7e3b07e20fff0b41ac3056094ee25c
djohnanderson <djohn@joingo.com>
2014-05-26 09:08:05 -0700

    add bitrate option use different temporary file on each run

--- makeVideo.py
+++ makeVideo.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
-import os, re, shutil, argparse
+import os, re, sys, shutil, argparse, tempfile
 from datetime import datetime
 
 parser = argparse.ArgumentParser(description='Make a video from JPEGs in a folder')
 parser.add_argument('-f','--folder', help='Folder of JPEGs', default="")
 parser.add_argument('-o','--output', help='Folder of JPEGs', default="")
+parser.add_argument('-b','--bitrate', help='bitrate in kilobits per second', default='2500')
 args = parser.parse_args()
 
 folder = args.folder
@@ -16,26 +17,37 @@ folder = folder + '/'
 
 if not os.path.isdir(folder):
     print "Can't open " + folder
-else:
-    output = args.output
-    if output == "":
-        output = folder + 'timelapse.mp4'
+    sys.exit(0)
 
-    files = os.listdir(folder)
-    files.sort()
+bitrate = args.bitrate
+try:
+    bitrate = int(bitrate)
+except ValueError:
+    print 'bitrate must be an integer'
+    sys.exit(0)
+bitrate = '-b %dk' % bitrate
 
-    regex = re.compile('^(image\d\d-\d\d-\d\d)')
-    shutil.rmtree('tmp', ignore_errors=True)
-    os.makedirs('tmp')
+output = args.output
+if output == "":
+    output = folder + 'timelapse.mp4'
 
-    index = 0
-    for filename in files:
-        filepath = folder + filename
-        if os.path.isfile(filepath):
-            match = regex.match(filename)
-            if match:
-                shutil.copy(filepath, 'tmp/image%04d.jpg' % index)
-                index = index + 1
+files = os.listdir(folder)
+files.sort()
 
-    os.system('/usr/bin/avconv -y -r 10 -i tmp/image\%04d.jpg -r 10 -b 2500k -vcodec libx264 ' + output)
+regex = re.compile('^(image\d\d-\d\d-\d\d)')
+tempDirectoryName = tempfile.mkdtemp()
+os.removedirs(tempDirectoryName)
+os.makedirs(tempDirectoryName)
+
+index = 0
+for filename in files:
+    filepath = folder + filename
+    if os.path.isfile(filepath):
+        match = regex.match(filename)
+        if match:
+            shutil.copy(filepath, tempDirectoryName + '/image%04d.jpg' % index)
+            index = index + 1
+
+os.system('/usr/bin/avconv -y -r 10 -i ' + tempDirectoryName + '/image\%04d.jpg -r 10 ' + bitrate + ' -vcodec libx264 ' + output)
+os.removedirs(tempDirectoryName)
 
